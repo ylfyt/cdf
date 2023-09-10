2fac670a8b88dfd40df25f848ec1f8ea548c4ef2
ylfyt <yalfayat@gmail.com>
2023-09-11 03:33:22 +0700

    Product service

--- services/product/main.go
+++ services/product/main.go
@@ -1,12 +1,61 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
 
+func get(conn *sql.DB, query string) ([]map[string]any, error) {
+	rows, err := conn.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	columns, err := rows.Columns()
+	if err != nil {
+		return nil, err
+	}
+
+	numOfColumns := len(columns)
+	scans := make([]any, numOfColumns)
+	scansPtr := make([]any, numOfColumns)
+
+	for i := range scans {
+		scansPtr[i] = &scans[i]
+	}
+
+	var result []map[string]any
+	for rows.Next() {
+		err := rows.Scan(scansPtr...)
+		if err != nil {
+			return nil, err
+		}
+		row := make(map[string]any)
+		for i, v := range columns {
+
+			row[v] = scans[i]
+		}
+		result = append(result, row)
+	}
+	return result, nil
+}
+
 func main() {
+	conStr := "postgresql://postgres:postgres@localhost/db_product?sslmode=disable"
+	db, err := sql.Open("postgres", conStr)
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := get(db, `SELECT * FROM product`)
+	if err != nil {
+		fmt.Println("Err", err)
+	}
+	fmt.Printf("Data: %+v\n", res)
+
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
