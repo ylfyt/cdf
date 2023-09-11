9ea9d4e3383d23429319dc3a9eef55f72f5bd3f5
ylfyt <yalfayat@gmail.com>
2023-09-11 09:55:59 +0700

    Product endpoint

--- services/product/main.go
+++ services/product/main.go
@@ -4,13 +4,14 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
 
-func get(conn *sql.DB, query string) ([]map[string]any, error) {
-	rows, err := conn.Query(query)
+func get(conn *sql.DB, query string, params ...any) ([]map[string]any, error) {
+	rows, err := conn.Query(query, params...)
 	if err != nil {
 		return nil, err
 	}
@@ -43,18 +44,18 @@ func get(conn *sql.DB, query string) ([]map[string]any, error) {
 	return result, nil
 }
 
+type ResponseDto struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+	Data    any    `json:"data"`
+}
+
 func main() {
 	conStr := "postgresql://postgres:postgres@localhost/db_product?sslmode=disable"
 	db, err := sql.Open("postgres", conStr)
 	if err != nil {
 		panic(err)
 	}
-
-	res, err := get(db, `SELECT * FROM product`)
-	if err != nil {
-		fmt.Println("Err", err)
-	}
-	fmt.Printf("Data: %+v\n", res)
 
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
@@ -64,10 +65,46 @@ func main() {
 	})
 
 	r.GET("/product", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"hehe": "dsadsa",
+		res, err := get(db, `SELECT * FROM product`)
+		if err != nil {
+			fmt.Println("Err", err)
+			ctx.JSON(http.StatusInternalServerError, ResponseDto{
+				Success: false,
+				Message: "",
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, ResponseDto{
+			Success: true,
+			Data:    res,
 		})
 	})
 
-	r.Run()
+	r.GET("/product/:id", func(ctx *gin.Context) {
+		idStr := ctx.Params.ByName("id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, ResponseDto{
+				Success: false,
+				Message: "id is not valid",
+			})
+		}
+		res, err := get(db, `SELECT * FROM product WHERE id = $1`, id)
+		if err != nil {
+			fmt.Println("Err", err)
+			ctx.JSON(http.StatusInternalServerError, ResponseDto{
+				Success: false,
+				Message: "",
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, ResponseDto{
+			Success: true,
+			Data:    res,
+		})
+	})
+
+	r.Run(":4000")
 }
